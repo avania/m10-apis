d67ab96072aed689d32d19c8016223e8fa640b8f
avania <avani.amin96@gmail.com>
2016-10-25 15:04:24 -0700

    Exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,11 +4,21 @@
 # This is a perfect situation in which writing a function will allow you to better structure your
 # code, and give a name to a repeated task.
 library(jsonlite)
-setwd('~/Documents/info-201/m10-apis/exercise-2/')
+setwd('~/Desktop//m10-apis/exercise-2/')
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
 
+TopTen <- function(input.artist) {
+  base <- 'https://api.spotify.com/v1/search?'
+  type <- '&type=artist'
+  search <- paste0(base, 'q=', input.artist, type)
+  artist.info <- fromJSON(artist.query)
+  artist.id <- artist.info$artists$items$id[1]
+  tracks.query <- paste0(base, 'artists/', artist.id, '/top-tracks?country=US')
+  tracks <- fromJSON(tracks.query)
+  return(tracks)
+}
 
 
 # What are the top 10 tracks by Nelly?
